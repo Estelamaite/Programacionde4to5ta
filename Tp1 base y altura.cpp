7fb0f22123c51210d5ec13852cc00c3a4bf36735
Estelamaite <164919108+Estelamaite@users.noreply.github.com>
2024-03-25 14:47:16 -0300

    Add files via upload

---
+++ Tp1 base y altura.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int base,altura,superficie;
+	
+	//entrada 
+	cout<<"ingrese base"<<endl;
+	cin>>base;
+	cout<<"ingrese altura"<<endl;
+	cin>>altura;
+	
+	//proceso
+	superficie=base*altura;
+	
+	//salida 
+	cout<<"la superficie es:"<<superficie<<endl;
+	
+
+    return 0;	
+}
