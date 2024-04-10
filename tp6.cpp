0d7bb10a208afb09a62a2e5a28ad18dd7eadfad5
Estela Maite <164919108+Estelamaite@users.noreply.github.com>
2024-04-10 20:43:32 -0300

    Add files via upload

---
+++ tp6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n1,n2;
+	cout<<"ingrese el primer numero"<<endl;
+	cin>>n1;
+	cout<<"ingrese el segundo numero"<<endl;
+	cin>>n2;
+	if(n1>=n2)
+	{
+		cout<<"el numero mas grande es:"<<n1;
+	}
+	else
+	{
+		cout<<"el numero mas grande es:"<<n2;
+	}
+	
+	return 0;
+}
