4b830dfd4785421e4934def7b4516027f1304014
Estela Maite <164919108+Estelamaite@users.noreply.github.com>
2024-04-22 14:32:39 -0300

    Add files via upload

---
+++ TRABAJO10.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int DDD= 0;
+    string cartel="DDD no encontrado ";
+    cout << "Ingrese el codigo de area: "<<endl;
+    cin >> DDD;
+    if(DDD==61) {
+    cartel= "El destino es Brasilia" ;
+    }
+    if(DDD == 71) {
+    cartel= "El destino es Salvador" ;
+    }  if(DDD == 11) {
+    cartel= "El destino es Sao Paulo";
+    }   if(DDD == 21) {
+    cartel= "El destino es Rio de Janeiro" ;
+    }   if(DDD == 32) {
+    cartel= "El destino es Juiz de Fora" ;
+    }   if(DDD == 19) {
+    cartel= "El destino es Campinas" ;
+    }   if(DDD == 27) {
+    cartel= "El destino es Vitoria" ;
+    }   if(DDD == 31) {
+    cartel= "El destino es Belo Horizonte" ;
+    }
+    cout<<cartel<<endl;
+
+    return 0;
+}
+
